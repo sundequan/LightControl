41ac4c86ce0679ec06f748ec282b90f63f0bab6b
sundequan <dequan_sun@163.com>
2018-06-10 18:55:35 +0800

    add jpa and security support

--- src/main/java/repo/sundq/light/controller/HttpSendController.java
+++ src/main/java/repo/sundq/light/controller/HttpSendController.java
@@ -4,12 +4,15 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import repo.sundq.light.json.HttpJsonResult;
+import repo.sundq.light.persistent.jpa.Device;
+import repo.sundq.light.persistent.jpa.DeviceRepository;
 import repo.sundq.light.pojo.HttpMsg;
 
 /**
@@ -23,10 +26,19 @@ import repo.sundq.light.pojo.HttpMsg;
 public class HttpSendController {
 	private Logger logger = LoggerFactory.getLogger(HttpSendController.class);
 	
+	@Autowired
+	private DeviceRepository deviceRepo;
+	
 	@RequestMapping(value="/send_test", produces="application/json;charset=utf-8")
 	public HttpJsonResult send(String msg) {
 		logger.info("get http send:" + msg);
 		return HttpJsonResult.build(200, "ok", null);
+	}
+	
+	@RequestMapping(value="/add_device", method=RequestMethod.POST, produces="application/json;charset=utf-8")
+	public HttpJsonResult add_device(Device device) {
+		deviceRepo.saveAndFlush(device);
+		return HttpJsonResult.build(200, "ok", device);
 	}
 	
 	@RequestMapping(value="/send", method=RequestMethod.POST, produces="application/json;charset=utf-8")
---
+++ src/main/java/repo/sundq/light/persistent/jpa/DeviceRepository.java
@@ -0,0 +1,12 @@
+package repo.sundq.light.persistent.jpa;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ *@auther ksun
+ *@date 2018年6月10日 下午6:02:56
+*/
+public interface DeviceRepository extends JpaRepository<Device, Integer> {
+	
+}
